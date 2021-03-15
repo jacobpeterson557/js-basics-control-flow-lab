041c856f5b10723809c93287e896c12fa95f3a28
+ <+>
2021-03-15 16:44:58 -0600

    commit tree 6a10c93ab43f95fe8fe577c2ce7aea43a9e57ce7

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(result){
+  if (result <= 400) {
+    return 'This one is on me!';
+  } else if (result >= 2000 && result < 2500){
+    return 'I will gladly take your thirty bucks.';
+  } else if (result >= 2500) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+scuberGreetingForFeet(199);
+scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2501);
+
+
+function ternaryCheckCity(capital){
+  return (capital === 'NYC' ? 'Ok, sounds good.' : 'No go.');
+}
+ternaryCheckCity('NYC');
+ternaryCheckCity('Pittsburgh');
+
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    case 'thanks for everything':
+      return 'Bye.';
+      break;  
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+switchOnCharmFromTip('generous');
+switchOnCharmFromTip('not as generous');
+switchOnCharmFromTip('thanks for everything');
